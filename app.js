4fd3d91facc66a7bec3ae16230a0f753abfb0a6e
Amanda Gois <amandabalcacar@gmail.com>
2024-03-29 14:39:30 -0400

    feat: banco de dados

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection.js');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 });
+
+//body-parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 //db connection
 db
@@ -18,8 +22,11 @@ db
         console.log("Ocorreu um erro ao conectar", err);
     })
 
-    
+
 //routes
 app.get('/', (req, res) => {
     res.send("Está funcionando 4");
 });
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
