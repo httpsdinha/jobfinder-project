b34ba333810939cb4ee13f2ba5d11c0bcdc3b3ce
Amanda Gois <amandabalcacar@gmail.com>
2024-03-31 16:59:49 -0400

    feat: creando rota com post

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const app = express();
-const db = require('./db/connection');
-
+const express       = require('express');
+const app           = express();
+const db            = require('./db/connection');
+const bodyParser    = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 });
+
+// body parser
+app.use(bodyParser.urlencoded({extended: false}));
 
 // db connection
 db
@@ -23,3 +26,6 @@ db
 app.get('/', (req, res) => {
     res.send("Está funcionando 3");
 });
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'));
