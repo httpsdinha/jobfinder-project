cdd2df692f9b78787186b4e14eca8c0bc2357282
Amanda Gois <amandabalcacar@gmail.com>
2024-03-31 16:21:01 -0400

    feat: conexão com banco de dados ok

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
+
 
 const PORT = 3000;
 
@@ -7,6 +9,17 @@ app.listen(PORT, function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 });
 
+// db connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao banco com sucesso");
+    })
+    .catch(err =>{
+        console.log("Ocorreu um erro ao conectar");
+    })
+
+// Routes
 app.get('/', (req, res) => {
     res.send("Está funcionando 3");
 });
