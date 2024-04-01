4a58abc8bb15d3c6d02c7c0ffc8e7bdb6d73c175
Amanda Gois <amandabalcacar@gmail.com>
2024-03-31 20:22:57 -0400

    feat: lógica de busca

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express       = require('express');
-const { engine }    = require('express-handlebars');
-const app           = express();
-const path          = require('path');
-const db            = require('./db/connection');
-const bodyParser    = require('body-parser');
-const Job           = require('./models/Job');
+const express    = require('express');
+const { engine } = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -36,14 +38,36 @@ db
 
 // Routes
 app.get('/', (req, res) => {
-    Job.findAll({order: [
-      ['createdAt', 'DESC']  
-    ]})
-    .then(jobs =>{
-        res.render('index', {
-            jobs
-        });
-    });
+
+    let search = req.query.job;
+    let query = '%'+search+'%';
+    
+    if(!search){
+        Job.findAll({order: [
+            ['createdAt', 'DESC']  
+          ]})
+          .then(jobs =>{
+              res.render('index', {
+                  jobs
+              });
+          })
+          .catch(err => console.log(err));
+    }else{
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+            ['createdAt', 'DESC']  
+          ]})
+          .then(jobs =>{
+            console.log(search);
+            console.log(search);
+              res.render('index', {
+                  jobs, search
+              });
+          })
+          .catch(err => console.log(err));
+    }
+  
     
 });
 
